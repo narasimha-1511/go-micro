c10ff1cbb7fb44a4fb2e7e604e99c90ff8db31a8
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-22 03:33:11 +0530

    added the redis as database and errror handling

--- application/app.go
+++ application/app.go
@@ -4,28 +4,70 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb *redis.Client
 }
 
 func New() *App {
+	
 	app := &App{
 		router: loadRoutes(),
+		rdb: redis.NewClient(&redis.Options{}),
 	}
+
 	return app
+
 }
+
+// to start the redis serverin windows --> run on the wsl follow redis documentation
 
 func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
 		Addr: ":3000",
 		Handler: a.router,
 	}
-	err:= server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to listen to the server: %w", err)
+
+	err:= a.rdb.Ping(ctx).Err()
+
+	return fmt.Errorf("failed to ping redis: %w", err)
 	}
-	return nil
+
+	defer func ()  {
+		err := a.rdb.Close()
+		if err != nil {
+			fmt.Println("failed to close redis connection", err)
+		}
+	}()
+
+	fmt.Println("Starting the server")
+
+	ch := make(chan error, 1)
+
+	go func ()  {
+
+		err= server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to listen to the server: %w", err) //publishin the error to the channel
+		}
+		close(ch)
+	}()
+
+	select {
+	case err := <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		return server.Shutdown(timeout)
+	}
+
+
+	// return nil
 
 }
--- main.go
+++ main.go
@@ -6,11 +6,24 @@ import (
 	"context"
 	"fmt"
 	"github.com/narasimha-1511/go-micro/application"
+
+	"os"
+	"os/signal"
 )
+
+// to start the redis server in windoes -> 
+// run it on wsl -> 
+// follow the redis documentation from the official website
+
 func main(){	
 	// TODO: Implement
 	app:= appilication.New();
-	error:= app.Start(context.TODO());
+	
+	ctx ,cancel := signal.NotifyContext(context.Background(),os.Interrupt);
+
+	defer cancel();
+
+	error:= app.Start(ctx);
 
 	if error!=nil{
 		fmt.Println("Error starting the application",error);
